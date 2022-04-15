452eb445c529f26db11b0b2b69afa93e440ab2ef
Lennart <lennart.aigner@tum.de>
2022-04-15 22:08:29 +0200

    feat: add singelton example

--- example_package/example_base_class.py
+++ example_package/example_base_class.py
@@ -3,13 +3,24 @@ from abc import ABC
 
 class BaseClass(ABC):
     __number_of_instances: int = 0
+    __initialized: bool = False
 
     def __init__(self) -> None:
         super().__init__()
         print("Init of BaseClass")
-        self.__number_of_instances += 1
+        self.__number_of_instances = self.__number_of_instances + 1
+        self.__initialized = True
 
     # For call to repr(). Prints object's information
     @classmethod
     def __repr__(self):
         return f"Current Number of instances of class {self.__name__}: {self.__number_of_instances}"
+
+    @classmethod
+    def init_ones(cls, *args, **kwargs):
+        if cls.__initialized:
+            print(f"{cls.__name__} is already initialized")
+            raise AssertionError
+        else:
+            cls.__initialized = True
+            return cls(args, kwargs)

28ce3b649a9f3bc026a7dc11bdb5f1c72f9dba6f
Lennart <lennart.aigner@tum.de>
2022-04-15 22:08:43 +0200

    chore: delete prints

--- bin/execute.py
+++ bin/execute.py
@@ -2,7 +2,11 @@ from example_package.example_class import ExampleClass
 
 if __name__ == "__main__":
     obj1 = ExampleClass("Lennart", 28)
-    print(obj1)
     obj2 = ExampleClass.from_string("Lennart", 1993)
-    print(obj1)
-    print(obj2.calculate_current_age(1993))
+
+    obj3 = ExampleClass.init_ones("Lennart", 29)
+
+    try:
+        obj4 = ExampleClass.init_ones("Lennart", 30)
+    except AssertionError:
+        print("Failed correctly as Exampled Class can only be initialized ones with init_ones")

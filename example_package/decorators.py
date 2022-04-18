91c34cf4b6fb0dfbd0a6937ded77be21da98436d
Lennart <lennart.aigner@tum.de>
2022-04-18 23:24:36 +0200

    feat: add @wraps to decorators

--- example_package/decorators.py
+++ example_package/decorators.py
@@ -1,8 +1,10 @@
 from typing import Optional, Callable
+from functools import wraps
 
 
 def print_hello_world(func):
 
+    @wraps(func)
     def wrapper(*args, **kwargs):
         print("Hello world")
         return func(*args, **kwargs)
@@ -14,6 +16,7 @@ def print_greeting(greeting: Optional[str] = "World"):
 
     def inner_decorator(func):
 
+        @wraps(func)
         def wrapper(*args, **kwargs):
             print(f"Hello {greeting}")
             return func(*args, **kwargs)
